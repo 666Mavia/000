e3b9ad314277214271f250a56e7ce6cd1c5e0c75
666Mavia <lotelak45378@gmail.com>
2024-09-10 17:59:43 -0400

    Update players_top10.js

--- players_top10.js
+++ players_top10.js
@@ -1,40 +1,17 @@
-var a1='Đạig3532';
-var b1='99f73d48-b468-4764-bda5-ae5fd4fdb408';
+var a1='Daven1980';
+var b1='eca04706-e644-4b19-be5c-22aedac6048f';
 var c1=0;
-var a2='NN';
-var b2='2208bd0b-2458-4c7a-80d4-2ebd4ad7c58d';
-var c2=1;
-var a3='5l夏特';
-var b3='870fcf2b-2a1f-4b68-b078-903a1504d7b6';
+var a2='Rhamaz';
+var b2='6314f7bc-9ff8-4f13-b192-61124c66925e';
+var c2=0;
+var a3='Риои';
+var b3='778d360b-96fd-4b81-925b-795c48ff50fb';
 var c3=0;
-var a4='DeV ';
-var b4='9b19e361-2512-4da2-8fcc-d7ccb437925c';
-var c4=0;
-var a5='Lo Nn';
-var b5='92bd6df9-0848-4483-8637-45890823a2f6';
+var a4='stasenci';
+var b4='edced443-4167-4e47-a7b9-b10b5e201d53';
+var c4=1;
+var a5='Steve K';
+var b5='ebe8fde5-1291-498f-a6be-38ee7f0aaf5b';
 var c5=0;
-var y6=162494;
-var z6=162486;
-//var n1='@666|BOGG';
-//var n2='Groth';
-//var n3='Axe Billionaire';
-//var n4='MobR';
-//var n5='Opto | BOGG 🇵🇭';
-//var p1='4b73046d-c461-48e2-9831-612fb8485f8b';
-//var p2='040b284d-ea59-45b8-904c-cba86cd119b6';
-//var p3='a2353be2-605e-4481-8fff-8daa15f1a762';
-//var p4='279022d4-3ca8-4da0-9cf4-d384d5cccad8';
-//var p5='bfd99f2d-64af-4849-97a3-a614cc93ccc0';
-//var b1=1;
-//var b2=1;
-//var b3=1;
-//var b4=1;
-//var b5=1;
-//var d0=162265;
-//var d1=162262;
-
-
-
-
-
-
+var y6=162708;
+var z6=162703;
